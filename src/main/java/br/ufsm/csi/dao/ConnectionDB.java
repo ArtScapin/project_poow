2ba519a602ffedf9f02356d5f148fa8bb5efe7a6
Arthur Scapin Pereira <arthurscapin19@gmail.com>
2023-06-12 20:21:14 -0300

    refactor

--- src/main/java/br/ufsm/csi/dao/ConnectionDB.java
+++ src/main/java/br/ufsm/csi/dao/ConnectionDB.java
@@ -2,10 +2,8 @@ package br.ufsm.csi.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
-public class ConectaDB {
+public class ConnectionDB {
     private static final String DRIVER = "org.postgresql.Driver";
     private static final String URL = "jdbc:postgresql://127.0.0.1:5432/project_poow";
     private static final String USER = "postgres";
--- src/main/java/br/ufsm/csi/until/Teste.java
+++ src/main/java/br/ufsm/csi/until/Teste.java
@@ -1,47 +1,11 @@
 package br.ufsm.csi.until;
 
-import br.ufsm.csi.dao.ConectaDB;
-import br.ufsm.csi.dao.UsuarioDAO;
+import br.ufsm.csi.dao.UserDAO;
 import br.ufsm.csi.model.Permissao;
 import br.ufsm.csi.model.Usuario;
 
 public class Teste {
     public static void main(String[] args) {
-        testaDeletar();
-    }
-
-    public static void testaDeletar() {
-        Usuario usuario = new UsuarioDAO().getUsuario(2);
-        if(usuario != null){
-            String status = new UsuarioDAO().deletar(usuario);
-            System.out.println(status);
-        }
-    }
-
-    public static void testaAtualizar() {
-        Usuario usuario = new UsuarioDAO().getUsuario(1);
-        if(usuario != null){
-            usuario.setEmail("teste@123");
-            String status = new UsuarioDAO().atualizar(usuario);
-            System.out.println(status);
-        }
-    }
-
-    public static void testaGetUsuario() {
-        new UsuarioDAO().getUsuario("joao@ufsm.br");
-    }
-
-    public static void testaCadastroUsuario() {
-        Permissao p = new Permissao(1, "MEDICO");
-        Usuario u = new Usuario("Paola", "paola@ufsm.br", "1234", true, p);
-        u.setPermissao(p);
-        new UsuarioDAO().cadastrar(u);
-    }
-
-    public static void testaGetUsuarios() {
-        for(Usuario u : new UsuarioDAO().getUsuarios()){
-            System.out.println("nome: " + u.getNome());
-            System.out.println("permissao: " + u.getPermissao().getNome());
-        }
+        System.out.println("OK");
     }
 }
